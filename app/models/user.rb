4d8a3670bf2936170586baea5e1d3e346eeb9d94
Alice Pike <alicepike123@hotmail.com>
2013-05-13 19:56:53 +0100

    user tests added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 		:message => "Username must be 3 or more characters" }
 	validates :username, :format => { :with => /\A[A-z]+\Z/,
 		:message => "Username can only contain letters a-z" }
+
 	
 	validate :password_must_contain_capital_letter_and_number, :on => :create
 
@@ -17,6 +18,17 @@ class User < ActiveRecord::Base
 			errors.add(:password, "Password must contain a number!")
 		end 
 	end
+
+	validate :username_must_start_with_a_capital_letter, :on => :create
+
+	def username_must_start_with_a_capital_letter
+		username_with_capital_letter = username[0..0] =~ /[A-Z]/
+		if username_with_capital_letter == nil
+			errors.add(:username, "Username must start with a capital letter!")
+		end
+	
+	end
+
 
 	has_and_belongs_to_many(:meetings)
 	has_many(:ratings)
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,14 @@
-require 'test_helper'
+require_relative '../test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "can't have a short name" do
+    user = User.new(username: "Alice")
+    refute user.valid?, "Should be fine"
+   end
+
+   test "username must start with a capital letter" do
+   	user = User.new(username: "daniel")
+   	refute user.valid?
+   	assert_includes user.errors, :username 
+   end
 end
